136f7aec1d10b2887248581dec00d3d96c938ea9
troxtele <indomisba@gmail.com>
2024-01-15 13:37:14 +0600

    d

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,24 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Gallery from "./components/Gallery";
 import Room from "./components/Room";
-import Calendar from "./components/Calendar";
+// import Calendar from "./components/Calendar";
 import Contact from "./components/Contact";
 import Map from "./components/Map";
 import Footer from "./components/Footer";
 import Guests from "./components/Guests";
+import { lazy, Suspense } from "react";
+import LoadingAnimation from "./ui/LoadingAnimation";
+
+const Calendar = lazy(() => import("./components/Calendar"));
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
-      <Calendar />
+      <Suspense fallback={<LoadingAnimation />}>
+        <Calendar />
+      </Suspense>
       <Gallery />
       <Room />
       <Guests />
